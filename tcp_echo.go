251d56fee73e4e1d3689c6fa49af711911dbc4b4
babul09 <babulbishwas09@gmail.com>
2026-06-24 19:11:38 +0530

    implemented routing

--- tcp_echo.go
+++ tcp_echo.go
@@ -27,6 +27,12 @@ func clientHandler() {
 	}
 }
 
+func SendRequest(conn net.Conn, status string, body string) {
+	getMessage := fmt.Sprintf("HTTP/1.1 %s\r\n"+"Content-Type: text/html\r\n"+"Content-Length: %v\r\n\r\n%s", status, len(body), body)
+	conn.Write([]byte(getMessage))
+
+}
+
 func handleConn(conn net.Conn) {
 	fmt.Printf("client connected :%v\n", conn.RemoteAddr())
 	defer conn.Close()
@@ -50,9 +56,27 @@ func handleConn(conn net.Conn) {
 	fmt.Printf("method = %v\npath = %v\nversion = %v\n", words[0], words[1], words[2])
 
 	if words[0] == "GET" {
-		getMessage := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 14\r\n\r\n<h1>Hello</h1>")
 
-		conn.Write(getMessage)
+		switch words[1] {
+		case "/":
+			{
+				SendRequest(conn, "200 OK", "<h1>hello<h1>")
+			}
+		case "/about":
+			{
+				getMessage := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 14\r\n\r\n<h1>About</h1>")
+
+				conn.Write(getMessage)
+
+			}
+		default:
+			{
+				getMessage := []byte("HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 12\r\n\r\n<h1>404</h1>")
+
+				conn.Write(getMessage)
+
+			}
+		}
 
 	}
 
