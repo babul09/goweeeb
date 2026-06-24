744772c55e15c74e60e1658b25f9edf11b0386a0
babul09 <babulbishwas09@gmail.com>
2026-06-24 14:38:47 +0530

    added method parsing

--- tcp_echo.go
+++ tcp_echo.go
@@ -1,8 +1,56 @@
 package main
 
-import(
+import (
+	// "bufio"
 	"fmt"
+	"io"
 	"net"
+	"strings"
+	// "os"
 )
 
-"
+func clientHandler() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("cant crete socket")
+
+	}
+
+	for {
+		conn, err := ln.Accept()
+
+		if err != nil {
+			fmt.Println("handshake unsuccessfulll")
+		}
+
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	fmt.Printf("client connected :%v\n", conn.RemoteAddr())
+	defer conn.Close()
+
+	data, err := io.ReadAll(conn)
+	if err != nil {
+		fmt.Println("some happend")
+	}
+
+	line := string(data)
+
+	line = strings.Split(line, "\r\n")[0]
+
+	words := strings.Fields(line)
+
+	if len(words) < 3 {
+		fmt.Println("invalid request")
+		return
+	}
+
+	fmt.Printf("method = %v\npath = %v\nversion = %v\n", words[0], words[1], words[2])
+
+}
+
+func main() {
+	clientHandler()
+}
