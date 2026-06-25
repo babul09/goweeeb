bd21c4e9603daba561ad88962b4fef6314ed168f
babul09 <babulbishwas09@gmail.com>
2026-06-26 03:34:26 +0530

    refactor: cleaned up the handleconn function

--- tcp_echo.go
+++ tcp_echo.go
@@ -27,9 +27,68 @@ func clientHandler() {
 	}
 }
 
-func SendResponse(conn net.Conn, status string, body string) {
+func sendResponse(conn net.Conn, status string, body string) {
 	getMessage := fmt.Sprintf("HTTP/1.1 %s\r\n"+"Content-Type: text/html\r\n"+"Content-Length: %v\r\n\r\n%s", status, len(body), body)
 	conn.Write([]byte(getMessage))
+
+}
+
+func handleGet(words []string, conn net.Conn) {
+	switch words[1] {
+	case "/":
+		{
+			sendResponse(conn, "200 OK", `<h1>Bhopdikeee</h1> 
+			<form method="POST" action="/submit">
+			<input name="username">
+			<button type="submit">send</button>	
+			</form>`)
+		}
+	case "/about":
+		{
+			sendResponse(conn, "200 OK", "<h1>about</h1>")
+		}
+	default:
+		{
+			sendResponse(conn, "404 Not Found", "<h1>404</h1>")
+		}
+	}
+
+}
+
+func handlePost(reader *bufio.Reader, conn net.Conn) {
+	contentLength := 0
+	// var err error
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("read error")
+		}
+
+		if line == "\r\n" {
+			body := make([]byte, contentLength)
+			_, err = reader.Read(body)
+			if err != nil {
+				fmt.Println("error reading body")
+			}
+			fmt.Println(string(body))
+
+			parts := strings.Split(string(body), "=")
+			if len(parts) != 2 {
+				sendResponse(conn, "400 Bad Request", "<h1>Bad Request</h1>")
+				return
+			}
+
+			userName := parts[1]
+			respo := fmt.Sprintf("<h1>Hello %s</h1>", userName)
+			sendResponse(conn, "200 OK", respo)
+			break
+		}
+
+		if strings.HasPrefix(line, "Content-Length") {
+			fmt.Sscanf(line, "Content-Length: %d", &contentLength)
+			fmt.Println(contentLength)
+		}
+	}
 
 }
 
@@ -51,59 +110,19 @@ func handleConn(conn net.Conn) {
 		return
 	}
 
-	ContentLength := 0
-
 	fmt.Printf("method = %v\npath = %v\nversion = %v\n", words[0], words[1], words[2])
 
-	if words[0] == "GET" {
+	switch words[0] {
 
-		switch words[1] {
-		case "/":
-			{
-				SendResponse(conn, "200 OK", `<h1>Bhopdikeee</h1> 
-			<form method="POST" action="/submit">
-			<input name="username">
-			<button type="submit">send</button>	
-			</form>`)
-			}
-		case "/about":
-			{
-				SendResponse(conn, "200 OK", "<h1>about</h1>")
-			}
-		default:
-			{
-				SendResponse(conn, "404 Not Found", "<h1>404</h1>")
-			}
+	case "GET":
+		{
+			handleGet(words, conn)
 		}
 
-	} else if words[0] == "POST" {
-		for {
-			line, err = reader.ReadString('\n')
-			if err != nil {
-				fmt.Println("read error")
-			}
-
-			if line == "\r\n" {
-				body := make([]byte, ContentLength)
-				_, err = reader.Read(body)
-				if err != nil {
-					fmt.Println("error reading body")
-				}
-				fmt.Println(string(body))
-
-				parts := strings.Split(string(body), "=")
-				userName := parts[1]
-				respo := fmt.Sprintf("<h1>Hello %s</h1>", userName)
-				SendResponse(conn, "200 OK", respo)
-				break
-			}
-
-			if strings.HasPrefix(line, "Content-Length") {
-				fmt.Sscanf(line, "Content-Length: %d", &ContentLength)
-				fmt.Println(ContentLength)
-			}
+	case "POST":
+		{
+			handlePost(reader, conn)
 		}
-
 	}
 
 }
